eba17b131203f20c6dba7046c0d9ac90636f3c85
leesumin <leesumin90@naver.com>
2022-11-25 15:00:55 +0900

    apply useInterval

--- timer/src/Timer.js
+++ timer/src/Timer.js
@@ -1,30 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Slider from '@mui/material/Slider';
 import useTimer from "./hooks/useTimer";
 
 function Timer() {
-    const [totalSec, minuteBar, handleMinuteBar, isRunning, isFinish, start, end] = useTimer(1);
+    const [restSeconds, setRestSeconds, isRunning, isFinish, start, end, reset] = useTimer(1);
 
     const handleSliderChange = (event, newMinute) => {
         if (typeof newMinute === 'number') {
-            handleMinuteBar(newMinute)
+            setRestSeconds(newMinute * 60);
         }
     };
 
     return (
         <>
-            <button onClick={() => { start(); }} disabled={isRunning}>시작</button>
-            <button onClick={() => { end(); }} disabled={!isRunning}>종료</button>            
-            <h1>{Math.floor(totalSec / 60)}분 {Math.floor(totalSec % 60)}초</h1>
+            <button onClick={() => { start(); }} disabled={isRunning || isFinish}>시작</button>
+            <button onClick={() => { end(); }} disabled={!isRunning}>종료</button>
+            <button onClick={() => { reset(); }} disabled={!isFinish}>초기화</button>
+            <h1>{Math.floor(restSeconds / 60)}분 {Math.floor(restSeconds % 60)}초</h1>
             <Slider
-                value={minuteBar}
                 step={1}
                 min={0}
                 max={60}
                 onChange={handleSliderChange}
                 aria-label="Disabled slider"
             />
-            { isFinish ? <h3>타이머 종료</h3> : null }
+            {isFinish ? <h3>타이머 종료</h3> : null}
         </>
     );
 }
--- timer/src/hooks/useTimer.js
+++ timer/src/hooks/useTimer.js
@@ -1,58 +1,41 @@
-import { useEffect, useRef, useState } from "react";
+import { useState } from "react";
+import useInterval from "./useInterval";
 
 const useTimer = (initialMinute) => {
-    console.log('useTimer');
-    const intervalId = useRef(0);
-    // const [intervalId, setIntervalId] = useState(0);
     const [isRunning, setIsRunning] = useState(false);
     const [isFinish, setIsFinish] = useState(false);
-    const [minuteBar, setMinuteBar] = useState(initialMinute);
-    const [totalSec, setTotalSec] = useState(initialMinute * 60);
+    const [restSeconds, setRestSeconds] = useState(initialMinute * 60);
+
+    useInterval(
+        () => { countdown() },
+        isRunning ? 1000 : null
+    );
+
+    const countdown = () => {
+        if (restSeconds <= 1) {
+            setIsRunning(false);
+            setIsFinish(true);
+        }
+        setRestSeconds(restSeconds - 1);
+    }
 
     const start = () => {
         setIsRunning(true);
         setIsFinish(false);
-        
-        intervalId.current = setInterval(() => {
-            setTotalSec(sec => {
-                if (sec <= 1) {
-                    setIsFinish(true);
-                    clearInterval(intervalId.current);
-                }
-                return sec - 1;
-            });
-        }, 1000);
-        // setIntervalId(setInterval(() => {
-        //     setTotalSec(sec => {
-        //         if (sec <= 1) {
-        //             setIsFinish(true);
-        //             clearInterval(intervalId.current);
-        //         }
-        //         return sec - 1;
-        //     });
-        // }, 1000));
     }
 
     const end = () => {
         setIsRunning(false);
         setIsFinish(false);
-        setMinuteBar(initialMinute);
-        setTotalSec(initialMinute * 60);
-        clearInterval(intervalId.current);
-        // clearInterval(intervalId);
     }
 
-    const handleMinuteBar = (newMinute) => {
-        setTotalSec(newMinute * 60);
-        setMinuteBar(newMinute);
+    const reset = () => {
+        setIsRunning(false);
+        setIsFinish(false);
+        setRestSeconds(initialMinute * 60);
     }
 
-    useEffect(() => {
-        return () => clearInterval(intervalId.current);
-        // return () => clearInterval(intervalId);
-    }, []);
-    
-    return [totalSec, minuteBar, handleMinuteBar, isRunning, isFinish, start, end];
+    return [restSeconds, setRestSeconds, isRunning, isFinish, start, end, reset];
 }
 
 export default useTimer;
