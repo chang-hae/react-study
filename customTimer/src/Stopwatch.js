6743b7f07bddcc663f3a4e1a844e2b7eb8d373ab
chang-hae <leesumin90@naver.com>
2023-04-26 11:18:19 +0900

    add onStart, onEnd

--- customTimer/src/Stopwatch.js
+++ customTimer/src/Stopwatch.js
@@ -1,9 +1,22 @@
 import React, { useState } from 'react';
 import useCustomStopwatch from './hooks/useCustomStopwatch';
 
+
 const Stopwatch = () => {
+    const [startTime, setStartTime] = useState(Date.now());
     const [expiry, setExpiry] = useState(300);
     const {seconds, isRunning, start, pause, reset} = useCustomStopwatch();
+
+    const onStart = () => {
+        const startMilli = Date.now();
+        setStartTime(startMilli);
+        console.log(startMilli);
+    }
+    
+    const onEnd = () => {
+        console.log(startTime);
+        console.log(startTime + (seconds * 1000));
+    }
 
     return (
         <div style={{ textAlign: 'center' }}>
@@ -11,9 +24,15 @@ const Stopwatch = () => {
                 <span>{seconds} : {expiry - seconds}</span>
             </div>
             <p>{isRunning ? 'Running' : 'Not running'}</p>
-            <button onClick={start}>Start</button>
+            <button onClick={() => {
+                start();
+                onStart();
+            }}>Start</button>
             <button onClick={pause}>Pause</button>
-            <button onClick={reset}>Reset</button>
+            <button onClick={() => {
+                reset();
+                onEnd();
+            }}>Reset</button>
         </div>
     );
 }
